9791fdc106fefc84832d3781e798b169a42bb8c0
valquiria03 <152810299+valquiria03@users.noreply.github.com>
2025-03-19 13:21:18 -0300

    Primeiro

---
+++ aula1/src/aula1/aula6.java
@@ -0,0 +1,27 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class aula6 {
+	
+	public static void main(String[] args) {
+	        Scanner leitor = new Scanner(System.in);
+	     
+	        
+	       int variavelA;
+	       int variavelB;
+	       
+	       
+	       System.out.println("Digite o valor da variável A");
+	       variavelA = leitor.nextInt();
+	       
+	        System.out.println("Digite o valor da variável B");
+	       variavelB = leitor.nextInt();
+	       
+	      
+	      
+	      System.out.println("Agora a variável A vale " + variavelB + " e a variável B vale "+ variavelA);
+	      
+	  }
+
+}
